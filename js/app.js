dd50576146d65ce15949fac2b129788e80004b4e
TomekMojsiuk <tomasz.mojsiuk@gmail.com>
2019-06-11 23:51:22 +0200

    webpack.config images path modified

--- js/app.js
+++ js/app.js
@@ -19,6 +19,8 @@ $(document).ready(function() {
   $(".loading__page").hide();
   navElement();
   hamburgerElement();
+  arrowDownElement();
+  arrowUpElement();
   socialMediaIconsElement();
   copyrightsElement();
   portfolio();
