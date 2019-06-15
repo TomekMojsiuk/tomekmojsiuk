bab60ee2b8c60d03ef0a476ae4e4d42f26502c3b
TomekMojsiuk <tomasz.mojsiuk@gmail.com>
2019-06-15 16:12:02 +0200

    portfolio projects updated and finished, elements display effects added

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,15 +1,19 @@
 const portfolio = () => {
-    //Projects
+ //Projects
  const invoiceMe = $(".invoice__me").find(".icon");
- const invoiceMeCodePreview = "https://github.com/TomekMojsiuk/invoice-generator";
+ const invoiceMeCodePreview =
+  "https://github.com/TomekMojsiuk/invoice-generator";
  const invoiceMeLivePreview = "https://invoice-me.netlify.com/";
 
  const weatherApp = $(".weather__app").find(".icon");
  const weatherAppCodePreview = "https://github.com/TomekMojsiuk/weather-app";
  const weatherLivePreview = "https://tm-weather.netlify.com/";
 
+ const tomekMojsiuk = $(".tomekmojsiuk").find(".icon");
+ const tomekMojsiukCodePreview = "https://github.com/TomekMojsiuk/tomekmojsiuk";
+
  const projectPreview = (projectName, codePreviewUrl, livePrevievUrl) => {
-  projectName.each(function(element, index) {
+  projectName.each(function(index) {
    $(this).on("click", function() {
     if (index === 0) {
      window.open(codePreviewUrl);
@@ -20,9 +24,9 @@ const portfolio = () => {
   });
  };
 
-projectPreview (invoiceMe, invoiceMeCodePreview, invoiceMeLivePreview);
-projectPreview (weatherApp, weatherAppCodePreview, weatherLivePreview);
-
+ projectPreview(invoiceMe, invoiceMeCodePreview, invoiceMeLivePreview);
+ projectPreview(weatherApp, weatherAppCodePreview, weatherLivePreview);
+ projectPreview(tomekMojsiuk, tomekMojsiukCodePreview, tomekMojsiukCodePreview);
 };
 
 export default portfolio;
