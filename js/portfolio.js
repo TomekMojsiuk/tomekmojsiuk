8911bc4a6e11e700bfe56f0d6e7a395aca8f8023
TomekMojsiuk <tomasz.mojsiuk@gmail.com>
2019-09-30 20:54:27 +0200

    portfolio links updated

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,37 +1,36 @@
 const portfolio = () => {
- //Projects
- const invoiceMe = $(".invoice__me").find(".icon");
- const invoiceMeCodePreview =
-  "https://github.com/TomekMojsiuk/invoice-generator";
- const invoiceMeLivePreview = "https://invoice-me.netlify.com/";
+  //Projects
+  const invoiceMe = $('.invoice__me').find('.icon');
+  const invoiceMeCodePreview = 'https://github.com/TomekMojsiuk/invoice-generator';
+  const invoiceMeLivePreview = 'https://invoice-me.netlify.com/';
 
- const weatherApp = $(".weather__app").find(".icon");
- const weatherAppCodePreview = "https://github.com/TomekMojsiuk/weather-app";
- const weatherLivePreview = "https://tm-weather.netlify.com/";
+  const weatherApp = $('.weather__app').find('.icon');
+  const weatherAppCodePreview = 'https://github.com/TomekMojsiuk/weather-app';
+  const weatherLivePreview = 'https://tm-weather.netlify.com/';
 
- const tomekMojsiuk = $(".tomekmojsiuk").find(".icon");
- const tomekMojsiukCodePreview = "https://github.com/TomekMojsiuk/tomekmojsiuk";
+  const tomekMojsiuk = $('.tomekmojsiuk').find('.icon');
+  const tomekMojsiukCodePreview = 'https://github.com/TomekMojsiuk/tomekmojsiuk';
 
- const soundEnglish = $(".soundenglish").find(".icon");
- const soundEnglishCodePreview = "https://github.com/TomekMojsiuk/sound-english";
- const soundEnglishLivePreview = "https://sound-english.netlify.com/";
+  const soundEnglish = $('.soundenglish').find('.icon');
+  const soundEnglishCodePreview = 'https://github.com/TomekMojsiuk/sound-english';
+  const soundEnglishLivePreview = 'https://soundenglish.pl/';
 
- const projectPreview = (projectName, codePreviewUrl, livePrevievUrl) => {
-  projectName.each(function(index) {
-   $(this).on("click", function() {
-    if (index === 0) {
-     window.open(codePreviewUrl);
-    } else {
-     window.open(livePrevievUrl);
-    }
-   });
-  });
- };
+  const projectPreview = (projectName, codePreviewUrl, livePrevievUrl) => {
+    projectName.each(function(index) {
+      $(this).on('click', function() {
+        if (index === 0) {
+          window.open(codePreviewUrl);
+        } else {
+          window.open(livePrevievUrl);
+        }
+      });
+    });
+  };
 
- projectPreview(invoiceMe, invoiceMeCodePreview, invoiceMeLivePreview);
- projectPreview(weatherApp, weatherAppCodePreview, weatherLivePreview);
- projectPreview(tomekMojsiuk, tomekMojsiukCodePreview, tomekMojsiukCodePreview);
- projectPreview(soundEnglish, soundEnglishCodePreview, soundEnglishLivePreview);
+  projectPreview(invoiceMe, invoiceMeCodePreview, invoiceMeLivePreview);
+  projectPreview(weatherApp, weatherAppCodePreview, weatherLivePreview);
+  projectPreview(tomekMojsiuk, tomekMojsiukCodePreview, tomekMojsiukCodePreview);
+  projectPreview(soundEnglish, soundEnglishCodePreview, soundEnglishLivePreview);
 };
 
 export default portfolio;
