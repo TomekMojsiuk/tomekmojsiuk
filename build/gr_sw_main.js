838caf78882c8c0c8f50780086689825976b49bf
Tomasz Mojsiuk <tomasz.mojsiuk@gmail.com>
2021-09-06 14:52:45 +0200

    Update gr_sw_main.js

--- build/gr_sw_main.js
+++ build/gr_sw_main.js
@@ -1,1 +1,2 @@
-importScripts(['https://ga.getresponse.com/wpn/gr_sw.js']);
+// importScripts(['https://ga.getresponse.com/wpn/gr_sw.js']);
+importScripts(['https://app-feature-autom-pv97io-ga.hq1-k8s.gr-dev.me/wpn/gr_sw.js']);
