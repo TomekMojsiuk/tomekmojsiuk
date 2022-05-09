acf2cd5c936ef20238f80c1b099250161311d41e
Tomasz Mojsiuk <tomasz.mojsiuk@gmail.com>
2022-05-09 14:25:25 +0200

    Update gr_sw_main.js

--- build/gr_sw_main.js
+++ build/gr_sw_main.js
@@ -1,2 +1,3 @@
-importScripts(['https://beta-ga.getresponse.com/wpn/gr_sw.js']);
+// importScripts(['https://beta-ga.getresponse.com/wpn/gr_sw.js']);
+importScripts(['https://ga.local.gr-dev.me/wpn/gr_sw.js']);
 
